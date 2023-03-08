0138a89c73661bea5b193d7cf6359cbb2005bf11
ChuchyX <jesuspenax11@gmail.com>
2023-03-08 17:12:48 -0500

    test completo mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,87 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
+ 
   title = 'Rock_Paper_Scissors-FM';
 
   noinit = true;
 
-  secondResult = true;
+  secondResult = false;
 
-  picked(choice: string)
+  youChoice = '';
+  cpuChoice = '';
+
+  result = 0;
+
+  score = 0;
+
+  playAgain()
+  {
+    this.noinit = true;
+    this.secondResult = false;
+  }
+
+  async picked(choice: string)
   {
     this.noinit = false;
-    console.log();
-    console.log(choice);
+    this.youChoice = choice;
+    const n = Math.floor(Math.random() * 3);
+    if(n === 0) this.cpuChoice = 'paper';
+    if(n === 1) this.cpuChoice = 'scissors';
+    if(n === 2) this.cpuChoice = 'rock';
+
+
+    if(choice === 'paper')
+    {
+      if(this.cpuChoice === 'paper') this.result = 0;
+      if(this.cpuChoice ==='scissors')
+      {
+        this.score--;
+        this.result = -1;
+      }
+      if(this.cpuChoice ==='rock')
+      {
+        this.score++;
+        this.result = 1;
+      }
+    }
+    if(choice === 'scissors')
+    {
+      if(this.cpuChoice === 'scissors') this.result = 0;
+      if(this.cpuChoice ==='paper')
+      {
+        this.score++;
+        this.result = 1;
+      }
+      if(this.cpuChoice ==='rock')
+      {
+        this.score--;
+        this.result = -1;
+      }
+    }
+    if(choice === 'rock')
+    {
+      if(this.cpuChoice === 'rock') this.result = 0;
+      if(this.cpuChoice ==='scissors')
+      {
+        this.score++;
+        this.result = 1;
+      }
+      if(this.cpuChoice ==='paper')
+      {
+        this.score--;
+        this.result = -1;
+      }
+    }
+    
+    await setTimeout(() => {
+      this.secondResult = true;
+    }, 1000);
+    
   }
 }
